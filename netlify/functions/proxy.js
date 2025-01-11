703cf49c87b986a93a3a6dfd47b100f267d8fd95
rijal <jeryzal333@gmail.com>
2025-01-11 18:17:46 +0700

    add response headers

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,35 +1,58 @@
-exports.handler = async (event) => {
-    const { path, queryStringParameters } = event;
-  
-    // // Target API endpoint
-    const apiEndpoint = 'https://binhusenstore.my.id'; 
-  
-    // Construct the full URL
-    const url = new URL(apiEndpoint);
-    // url.pathname = path.replace(/^\/api/, ''); // Remove leading "/api"
-    url.pathname = path.replace(".netlify/functions/proxy/", ''); // Remove leading "/api"
-    if (queryStringParameters) {
-      url.search = new URLSearchParams(queryStringParameters).toString();
-    }
-  
-    console.log("the url: ", url.toString())
-    try {
-      const response = await fetch(url.toString());
-      const data = await response.json();
-  
-      return {
-        statusCode: response.status,
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          ...response.headers
-        },
-      };
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'Error fetching data' }),
-      };
-    }
+exports.handler = async (event, context) => {
+  const { method, headers, body, path } = event;
+  const url = new URL(event.rawUrl.replace("/.netlify/functions/proxy", ""));
+
+  const response = await fetch(url, {
+    method,
+    headers,
+    body,
+  });
+
+  const bodyResponse = await response.json();
+
+  return {
+    statusCode: response.status,
+    headers: {
+      'Content-Type': 'application/json',
+      'Set-cookie': response.headers.getSetCookie(),
+      'Refresh': response.headers.get("refresh")
+    },
+    body: bodyResponse,
   };
+};
+
+// exports.handler = async (event) => {
+//     const { path, queryStringParameters } = event;
+  
+//     // // Target API endpoint
+//     const apiEndpoint = 'https://binhusenstore.my.id'; 
+  
+//     // Construct the full URL
+//     const url = new URL(apiEndpoint);
+//     // url.pathname = path.replace(/^\/api/, ''); // Remove leading "/api"
+//     url.pathname = path.replace(".netlify/functions/proxy/", ''); // Remove leading "/api"
+//     if (queryStringParameters) {
+//       url.search = new URLSearchParams(queryStringParameters).toString();
+//     }
+  
+//     console.log("the url: ", url.toString())
+//     try {
+//       const response = await fetch(url.toString());
+//       const data = await response.json();
+  
+//       return {
+//         statusCode: response.status,
+//         body: JSON.stringify(data),
+//         headers: {
+//           'Content-Type': 'application/json',
+//           ...response.headers
+//         },
+//       };
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ message: 'Error fetching data' }),
+//       };
+//     }
+//   };
